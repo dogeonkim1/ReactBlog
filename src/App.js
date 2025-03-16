02c6ace2c84109d22d926febb6c213c842a1bfd7
dogeonkim1 <caddibo4@naver.com>
2025-03-16 17:11:16 +0900

    style: posts 변수 저장

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+
+let posts = { color : 'blue', fontSize : '30px'}
+    let content = '강남 고기 맛집'
   return (
     <div className="App">
-      <div className="App">
           <div className="black-nav">
-              <div>개발 Blog</div>
+              <div style={posts}>개발 Blog</div>
           </div>
-      </div>
+          <h4> { content } </h4>
     </div>
   );
 }
